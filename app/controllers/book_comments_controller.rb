3f2d8a49f5c938733ec3d8251a7601b96b2b3170
ui-a <135043176+ui-a@users.noreply.github.com>
2023-06-28 06:33:52 +0000

    bookers2-debug-3finish

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,11 +1,15 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    if @book_comment.save
+      redirect_to book_path(@book)
+    else
+      @user = @book.user
+      render "books/show"
+    end
   end
 
 
