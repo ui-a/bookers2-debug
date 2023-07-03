4b2959291cef355623b2807004e360f40608094a
ui-a <135043176+ui-a@users.noreply.github.com>
2023-07-03 04:34:42 +0000

    bookers2-debug4-2

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,12 +1,13 @@
 class RelationshipsController < ApplicationController
 
   def create
+    @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
     redirect_to request.referer
   end
 
   def destroy
-    urrent_user.unfollow(params[:user_id])
+    current_user.unfollow(params[:user_id])
     redirect_to request.referer
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,11 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  # 自分がフォローしたりアンフォローしたりするための記述
+  # フォローアンフォロー
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  # フォロー一覧
-  has_many :followings, through: :active_relationships, source: :follower
-
-  # フォローされる方
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  # フォロワー一覧
+  # フォロー・フォロワー一覧
+  has_many :followings, through: :active_relationships, source: :follower
   has_many :followers, through: :passive_relationships, source: :followed
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -28,15 +25,16 @@ class User < ApplicationRecord
   end
 
   def follow(user_id)
-    active_relationships.create(follower_id: user_id)
+    active_relationships.create(followed_id: user_id)
   end
 
   def unfollow(user_id)
-    active_relationships.find_by(follower_id: user_id).destroy
+    active_relationships.find_by(followed_id: user_id).destroy
   end
 
   def following?(user)
     followings.include?(user)
   end
+
 
 end
